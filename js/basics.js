de77a63a2256d21c4b1d762f5c244f1454c71cb0
sodamachinebroke <79090166+sodamachinebroke@users.noreply.github.com>
2023-12-06 20:37:29 +0100

    Commented

--- js/basics.js
+++ js/basics.js
@@ -7,16 +7,20 @@ class GamePlay extends Phaser.Scene{
     }
 
     init(){
+
+        //Basic statistics
         this.stats = {
             cigi: 1,
             onb: 1
         }
 
+        //Requirements to win, these are temporary
         this.winReqs = {
             cigi: 10,
             onb: 10
         }
 
+        //These are interaction options
         this.actions = [
             {
                 key: 'Tarhálsz',
@@ -49,6 +53,7 @@ class GamePlay extends Phaser.Scene{
 
     create(){
 
+        //Temporary to track certain variables
         this.cigiText = this.add.text(20, 50, `Cigi: ${this.stats.cigi}`,{
             font: '24px Arial',
             color: 'white'
@@ -60,7 +65,7 @@ class GamePlay extends Phaser.Scene{
         });
 
         
-
+        //Debug purposes
         this.input.keyboard.on('keydown-B', () => {
             this.updateCigi(1);
         });
@@ -69,6 +74,8 @@ class GamePlay extends Phaser.Scene{
             this.updateCigi(-1);
         });
 
+
+        //This is where science stopped lmao
         this.options = this.add.group(this.actions);
 
         Phaser.Actions.Call(this.options.getChildren(), (option) => {
@@ -111,6 +118,7 @@ class GamePlay extends Phaser.Scene{
 
     };
 
+    //This might actually be final
     updateCigi(number){
         if (this.stats.cigi > 0){
             this.stats.cigi += number;
